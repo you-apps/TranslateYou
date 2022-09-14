a342e6bcda3aa11569f34835ab461481d6726969
Bnyro <yannick.fehler@web.de>
2022-09-14 15:43:05 +0200

    add bottom divider

--- app/src/main/java/com/bnyro/translate/ui/MainContent.kt
+++ app/src/main/java/com/bnyro/translate/ui/MainContent.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -61,7 +62,16 @@ fun MainContent(
                 StyledTextField(
                     text = mainModel.translatedText,
                     onValueChange = {},
-                    readOnly = true
+                    readOnly = true,
+                    modifier = Modifier
+                        .weight(1.0f)
+                )
+
+                Divider(
+                    color = Color.Gray,
+                    modifier = Modifier
+                        .align(alignment = Alignment.CenterHorizontally)
+                        .size(70.dp, 2.dp)
                 )
             }
         }
