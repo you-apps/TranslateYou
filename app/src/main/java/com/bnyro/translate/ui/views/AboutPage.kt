d39991997d09030368979c7618f15b2bd14c3bd1
Bnyro <yannick.fehler@web.de>
2022-09-15 18:50:30 +0200

    fixes

--- app/src/main/java/com/bnyro/translate/ui/views/AboutPage.kt
+++ app/src/main/java/com/bnyro/translate/ui/views/AboutPage.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -34,7 +35,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.LaunchedEffect
@@ -57,12 +60,14 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.bnyro.translate.BuildConfig
 import com.bnyro.translate.R
+import com.bnyro.translate.models.NavigationModel
 import com.bnyro.translate.ui.components.FeedbackIconButton
-import com.bnyro.translate.ui.components.StyledScaffold
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutPage() {
+fun AboutPage(
+    viewModel: NavigationModel = androidx.lifecycle.viewmodel.compose.viewModel()
+) {
     val context = LocalContext.current
     val view = LocalView.current
     var currentVersion by remember { mutableStateOf("") }
@@ -73,157 +78,169 @@ fun AboutPage() {
         currentVersion = BuildConfig.VERSION_CODE.toString()
     }
 
-    StyledScaffold(
-        containerColor = MaterialTheme.colorScheme.surface,
-        navigationIcon = {
-            FeedbackIconButton(
-                imageVector = Icons.Rounded.ArrowBack,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurface
-            )
-        },
-        actions = {
-            FeedbackIconButton(
-                modifier = Modifier.size(20.dp),
-                imageVector = Icons.Rounded.Balance,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurface
+    Scaffold(
+        modifier = Modifier
+            .statusBarsPadding(),
+        topBar = {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    FeedbackIconButton(
+                        imageVector = Icons.Rounded.ArrowBack,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurface
+                    ) {
+                        viewModel.showAbout = false
+                    }
+                },
+                actions = {
+                    FeedbackIconButton(
+                        modifier = Modifier.size(20.dp),
+                        imageVector = Icons.Rounded.Balance,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
             )
         },
         content = {
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
-            ) {
-                item {
-                    Column(
-                        modifier = Modifier.pointerInput(Unit) {
-                            detectTapGestures(
-                                onPress = {
-                                    view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
-                                    pressAMP = 0f
-                                    tryAwaitRelease()
-                                    view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
-                                    view.playSoundEffect(SoundEffectConstants.CLICK)
-                                    pressAMP = 16f
-                                },
-                                onTap = {
-                                    clickTime = if (System.currentTimeMillis() - clickTime > 2000) {
-                                        System.currentTimeMillis()
-                                    } else {
-                                        System.currentTimeMillis()
+            Column {
+                Spacer(modifier = Modifier.height(it.calculateTopPadding()))
+
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    item {
+                        Column(
+                            modifier = Modifier.pointerInput(Unit) {
+                                detectTapGestures(
+                                    onPress = {
+                                        view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+                                        pressAMP = 0f
+                                        tryAwaitRelease()
+                                        view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+                                        view.playSoundEffect(SoundEffectConstants.CLICK)
+                                        pressAMP = 16f
+                                    },
+                                    onTap = {
+                                        clickTime = if (System.currentTimeMillis() - clickTime > 2000) {
+                                            System.currentTimeMillis()
+                                        } else {
+                                            System.currentTimeMillis()
+                                        }
+                                    }
+                                )
+                            },
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .size(240.dp)
+                                    .background(
+                                        color = MaterialTheme.colorScheme.primaryContainer,
+                                        shape = RoundedCornerShape(30.dp)
+                                    )
+                                    .shadow(
+                                        elevation = 10.dp,
+                                        shape = RoundedCornerShape(30.dp),
+                                        ambientColor = MaterialTheme.colorScheme.primaryContainer,
+                                        spotColor = MaterialTheme.colorScheme.primaryContainer
+                                    ),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Image(
+                                    modifier = Modifier.size(90.dp),
+                                    painter = painterResource(R.mipmap.ic_launcher),
+                                    contentDescription = stringResource(R.string.app_name),
+                                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(48.dp))
+                            BadgedBox(
+                                badge = {
+                                    Badge(
+                                        modifier = Modifier.animateContentSize(tween(800)),
+                                        containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                                        contentColor = MaterialTheme.colorScheme.tertiary
+                                    ) {
+                                        Text(text = currentVersion)
                                     }
                                 }
-                            )
-                        },
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .size(240.dp)
-                                .background(
-                                    color = MaterialTheme.colorScheme.primaryContainer,
-                                    shape = RoundedCornerShape(30.dp)
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.app_name),
+                                    style = MaterialTheme.typography.displaySmall
                                 )
-                                .shadow(
-                                    elevation = 10.dp,
-                                    shape = RoundedCornerShape(30.dp),
-                                    ambientColor = MaterialTheme.colorScheme.primaryContainer,
-                                    spotColor = MaterialTheme.colorScheme.primaryContainer
-                                ),
-                            contentAlignment = Alignment.Center
+                            }
+                        }
+                    }
+                    item {
+                        Spacer(modifier = Modifier.height(48.dp))
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Image(
-                                modifier = Modifier.size(90.dp),
-                                painter = painterResource(R.mipmap.ic_launcher),
-                                contentDescription = stringResource(R.string.app_name),
-                                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                            // Sponsor
+                            RoundIconButton(
+                                RoundIconButtonType.Sponsor(
+                                    backgroundColor = MaterialTheme.colorScheme.tertiaryContainer
+                                ) {
+                                    view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+                                    view.playSoundEffect(SoundEffectConstants.CLICK)
+                                }
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+
+                            // GitHub
+                            RoundIconButton(
+                                RoundIconButtonType.GitHub(
+                                    backgroundColor = MaterialTheme.colorScheme.primaryContainer
+                                ) {
+                                    view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+                                    view.playSoundEffect(SoundEffectConstants.CLICK)
+                                    context.startActivity(
+                                        Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse("")
+                                        )
+                                    )
+                                }
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+
+                            // Telegram
+                            RoundIconButton(
+                                RoundIconButtonType.Telegram(
+                                    backgroundColor = MaterialTheme.colorScheme.primaryContainer
+                                ) {
+                                    view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+                                    view.playSoundEffect(SoundEffectConstants.CLICK)
+                                    context.startActivity(
+                                        Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse("")
+                                        )
+                                    )
+                                }
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+
+                            // Help
+                            RoundIconButton(
+                                RoundIconButtonType.Help(
+                                    backgroundColor = MaterialTheme.colorScheme.secondaryContainer
+                                ) {
+                                    view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+                                    view.playSoundEffect(SoundEffectConstants.CLICK)
+                                }
                             )
                         }
                         Spacer(modifier = Modifier.height(48.dp))
-                        BadgedBox(
-                            badge = {
-                                Badge(
-                                    modifier = Modifier.animateContentSize(tween(800)),
-                                    containerColor = MaterialTheme.colorScheme.tertiaryContainer,
-                                    contentColor = MaterialTheme.colorScheme.tertiary
-                                ) {
-                                    Text(text = currentVersion)
-                                }
-                            }
-                        ) {
-                            Text(
-                                text = stringResource(R.string.app_name),
-                                style = MaterialTheme.typography.displaySmall
-                            )
-                        }
                     }
-                }
-                item {
-                    Spacer(modifier = Modifier.height(48.dp))
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        // Sponsor
-                        RoundIconButton(
-                            RoundIconButtonType.Sponsor(
-                                backgroundColor = MaterialTheme.colorScheme.tertiaryContainer
-                            ) {
-                                view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
-                                view.playSoundEffect(SoundEffectConstants.CLICK)
-                            }
-                        )
-                        Spacer(modifier = Modifier.width(16.dp))
-
-                        // GitHub
-                        RoundIconButton(
-                            RoundIconButtonType.GitHub(
-                                backgroundColor = MaterialTheme.colorScheme.primaryContainer
-                            ) {
-                                view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
-                                view.playSoundEffect(SoundEffectConstants.CLICK)
-                                context.startActivity(
-                                    Intent(
-                                        Intent.ACTION_VIEW,
-                                        Uri.parse("")
-                                    )
-                                )
-                            }
-                        )
-                        Spacer(modifier = Modifier.width(16.dp))
-
-                        // Telegram
-                        RoundIconButton(
-                            RoundIconButtonType.Telegram(
-                                backgroundColor = MaterialTheme.colorScheme.primaryContainer
-                            ) {
-                                view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
-                                view.playSoundEffect(SoundEffectConstants.CLICK)
-                                context.startActivity(
-                                    Intent(
-                                        Intent.ACTION_VIEW,
-                                        Uri.parse("")
-                                    )
-                                )
-                            }
-                        )
-                        Spacer(modifier = Modifier.width(16.dp))
-
-                        // Help
-                        RoundIconButton(
-                            RoundIconButtonType.Help(
-                                backgroundColor = MaterialTheme.colorScheme.secondaryContainer
-                            ) {
-                                view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
-                                view.playSoundEffect(SoundEffectConstants.CLICK)
-                            }
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(48.dp))
                 }
             }
         }
