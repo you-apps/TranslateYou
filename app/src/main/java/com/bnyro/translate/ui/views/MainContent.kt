5bea6842f4317e32ea29490247ef014b0e89b7e5
Bnyro <yannick.fehler@web.de>
2022-09-18 17:54:59 +0200

    add a floating action button for making a new translation

--- app/src/main/java/com/bnyro/translate/ui/views/MainContent.kt
+++ app/src/main/java/com/bnyro/translate/ui/views/MainContent.kt
@@ -1,27 +1,37 @@
 package com.bnyro.translate.ui.views
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ContentPaste
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bnyro.translate.R
 import com.bnyro.translate.models.MainModel
@@ -33,6 +43,10 @@ import com.bnyro.translate.util.ClipboardHelper
 fun MainContent(
     viewModel: MainModel = viewModel()
 ) {
+    val focusRequester = remember {
+        FocusRequester()
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -42,79 +56,112 @@ fun MainContent(
             modifier = Modifier
                 .weight(1.0f)
         ) {
-            Column(
-                modifier = Modifier
-                    .padding(20.dp)
-                    .fillMaxSize()
-            ) {
-                StyledTextField(
-                    text = viewModel.insertedText,
-                    onValueChange = {
-                        viewModel.insertedText = it
-                        viewModel.enqueueTranslation()
-                    },
-                    maxLines = 8,
-                    placeholder = stringResource(R.string.enter_text)
-                )
-
-                Divider(
-                    color = MaterialTheme.colorScheme.onSurface,
+            Box {
+                Column(
                     modifier = Modifier
-                        .padding(10.dp)
-                        .align(alignment = Alignment.CenterHorizontally)
-                        .size(70.dp, 1.dp)
-                )
-
-                val clipboardHelper = ClipboardHelper(
-                    LocalContext.current.applicationContext
-                )
-                val copiedText = clipboardHelper.get()
-
-                if (copiedText != null && viewModel.insertedText == "") {
-                    Button(
-                        onClick = {
-                            viewModel.insertedText = copiedText
+                        .padding(15.dp)
+                        .fillMaxSize()
+                ) {
+                    StyledTextField(
+                        text = viewModel.insertedText,
+                        onValueChange = {
+                            viewModel.insertedText = it
                             viewModel.enqueueTranslation()
                         },
+                        maxLines = 8,
+                        placeholder = stringResource(R.string.enter_text),
+                        modifier = Modifier.focusRequester(focusRequester)
+                    )
+
+                    Divider(
+                        color = MaterialTheme.colorScheme.onSurface,
                         modifier = Modifier
-                            .padding(15.dp, 0.dp)
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically
+                            .padding(10.dp)
+                            .align(alignment = Alignment.CenterHorizontally)
+                            .size(70.dp, 1.dp)
+                    )
+
+                    val clipboardHelper = ClipboardHelper(
+                        LocalContext.current.applicationContext
+                    )
+                    val copiedText = clipboardHelper.get()
+
+                    if (copiedText != null && viewModel.insertedText == "") {
+                        Button(
+                            onClick = {
+                                viewModel.insertedText = copiedText
+                                viewModel.enqueueTranslation()
+                            },
+                            modifier = Modifier
+                                .padding(15.dp, 0.dp)
                         ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Icon(
+                                    Icons.Default.ContentPaste,
+                                    null,
+                                    modifier = Modifier
+                                        .size(18.dp)
+                                )
+                                Text(
+                                    stringResource(
+                                        id = R.string.paste
+                                    ),
+                                    modifier = Modifier
+                                        .padding(6.dp, 0.dp, 0.dp, 0.dp)
+                                )
+                            }
+                        }
+                    }
+
+                    StyledTextField(
+                        text = viewModel.translatedText,
+                        onValueChange = {},
+                        readOnly = true,
+                        modifier = Modifier
+                            .weight(1.0f)
+                    )
+
+                    Divider(
+                        color = Color.Gray,
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterHorizontally)
+                            .size(70.dp, 2.dp)
+                    )
+                }
+
+                if (viewModel.insertedText != "") {
+                    ExtendedFloatingActionButton(
+                        onClick = {
+                            viewModel.insertedText = ""
+                            viewModel.translatedText = ""
+                            focusRequester.requestFocus()
+                        },
+                        modifier = Modifier.align(
+                            alignment = Alignment.BottomEnd
+                        )
+                            .padding(15.dp, 40.dp)
+                    ) {
+                        Row {
                             Icon(
-                                Icons.Default.ContentPaste,
-                                null,
-                                modifier = Modifier
-                                    .size(18.dp)
+                                Icons.Default.Add,
+                                null
                             )
+                            Spacer(
+                                Modifier.width(10.dp)
+                            )
+
                             Text(
-                                stringResource(
-                                    id = R.string.paste
-                                ),
-                                modifier = Modifier
-                                    .padding(6.dp, 0.dp, 0.dp, 0.dp)
+                                stringResource(R.string.new_translation),
+                                fontSize = 16.sp
                             )
                         }
                     }
                 }
-
-                StyledTextField(
-                    text = viewModel.translatedText,
-                    onValueChange = {},
-                    readOnly = true,
-                    modifier = Modifier
-                        .weight(1.0f)
-                )
-
-                Divider(
-                    color = Color.Gray,
-                    modifier = Modifier
-                        .align(alignment = Alignment.CenterHorizontally)
-                        .size(70.dp, 2.dp)
-                )
             }
         }
+
         Row(
             modifier = Modifier
                 .padding(10.dp),
@@ -157,4 +204,10 @@ fun MainContent(
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun MainContentPreview() {
+    MainContent()
 }
