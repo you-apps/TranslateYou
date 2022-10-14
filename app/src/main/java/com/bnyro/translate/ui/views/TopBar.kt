582dd7bcc8774ba43b32aa7040c2aad8b103bfac
Bnyro <bnyro@tutanota.com>
2022-10-14 14:40:49 +0200

    add speech recognition

--- app/src/main/java/com/bnyro/translate/ui/views/TopBar.kt
+++ app/src/main/java/com/bnyro/translate/ui/views/TopBar.kt
@@ -1,9 +1,14 @@
 package com.bnyro.translate.ui.views
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.speech.SpeechRecognizer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -13,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bnyro.translate.R
 import com.bnyro.translate.obj.MenuItemData
@@ -20,6 +26,7 @@ import com.bnyro.translate.ui.components.StyledIconButton
 import com.bnyro.translate.ui.components.TopBarMenu
 import com.bnyro.translate.ui.models.MainModel
 import com.bnyro.translate.util.ClipboardHelper
+import com.bnyro.translate.util.SpeechHelper
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -36,6 +43,27 @@ fun TopBar(
             )
         },
         actions = {
+            if (mainModel.insertedText == "" && SpeechRecognizer.isRecognitionAvailable(context)) {
+                StyledIconButton(
+                    imageVector = Icons.Default.Mic
+                ) {
+                    if (
+                        ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.RECORD_AUDIO
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        SpeechHelper.checkPermission(context as Activity)
+                        return@StyledIconButton
+                    }
+
+                    SpeechHelper.recognizeSpeech(context as Activity) {
+                        mainModel.insertedText = it
+                        mainModel.enqueueTranslation()
+                    }
+                }
+            }
+
             if (mainModel.translatedText != "") {
                 StyledIconButton(
                     imageVector = Icons.Default.ContentCopy,
---
+++ app/src/main/java/com/bnyro/translate/util/SpeechHelper.kt
@@ -0,0 +1,74 @@
+package com.bnyro.translate.util
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import androidx.core.app.ActivityCompat
+import java.util.*
+
+object SpeechHelper {
+    fun checkPermission(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                0
+            )
+        }
+    }
+
+    fun recognizeSpeech(activity: Activity, onResult: (String) -> Unit) {
+        val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(activity)
+        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(params: Bundle?) {
+            }
+
+            override fun onBeginningOfSpeech() {
+            }
+
+            override fun onRmsChanged(rmsdB: Float) {
+            }
+
+            override fun onBufferReceived(buffer: ByteArray?) {
+            }
+
+            override fun onEndOfSpeech() {
+            }
+
+            override fun onError(error: Int) {
+            }
+
+            override fun onResults(results: Bundle?) {
+                val data = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                data?.first()?.let { onResult.invoke(it) }
+            }
+
+            override fun onPartialResults(partialResults: Bundle?) {
+                val data = partialResults?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                data?.first()?.let { onResult.invoke(it) }
+            }
+
+            override fun onEvent(eventType: Int, params: Bundle?) {
+            }
+        })
+
+        val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        speechRecognizer.startListening(speechRecognizerIntent)
+
+        speechRecognizerIntent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        speechRecognizerIntent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE,
+            Locale.getDefault()
+        )
+
+        activity.startActivity(speechRecognizerIntent)
+    }
+}
