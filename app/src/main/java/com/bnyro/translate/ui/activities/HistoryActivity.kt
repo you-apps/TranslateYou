b827645370af7de92e446c8990547c48cbc00188
Bnyro <bnyro@tutanota.com>
2022-10-17 15:12:05 +0200

    Limit the history text length to 3 lines and show a dialog with the full translation on click (closes #48)

--- app/src/main/java/com/bnyro/translate/ui/activities/HistoryActivity.kt
+++ app/src/main/java/com/bnyro/translate/ui/activities/HistoryActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
--- app/src/main/java/com/bnyro/translate/ui/views/HistoryRow.kt
+++ app/src/main/java/com/bnyro/translate/ui/views/HistoryRow.kt
@@ -1,5 +1,7 @@
 package com.bnyro.translate.ui.views
 
+import android.app.Activity
+import android.content.Intent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,12 +11,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bnyro.translate.R
 import com.bnyro.translate.db.obj.HistoryItem
+import com.bnyro.translate.ui.activities.MainActivity
 import com.bnyro.translate.ui.components.StyledIconButton
 
 @Composable
@@ -22,11 +35,19 @@ fun HistoryRow(
     historyItem: HistoryItem,
     onDelete: () -> Unit
 ) {
+    val context = LocalContext.current
+
+    var showDialog by remember {
+        mutableStateOf(
+            false
+        )
+    }
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .clickable {
-                // TODO() Start Main Activity with data
+                showDialog = true
             }
             .padding(
                 start = 15.dp,
@@ -42,7 +63,9 @@ fun HistoryRow(
         ) {
             Text(
                 historyItem.insertedText,
-                fontSize = 18.sp
+                fontSize = 18.sp,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
             )
 
             Spacer(
@@ -52,7 +75,9 @@ fun HistoryRow(
 
             Text(
                 historyItem.translatedText,
-                fontSize = 14.sp
+                fontSize = 14.sp,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
             )
         }
         StyledIconButton(
@@ -60,5 +85,51 @@ fun HistoryRow(
         ) {
             onDelete.invoke()
         }
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = {
+                showDialog = false
+            },
+            text = {
+                Column {
+                    Text(
+                        historyItem.insertedText,
+                        fontSize = 18.sp
+                    )
+
+                    Spacer(
+                        modifier = Modifier
+                            .height(5.dp)
+                    )
+
+                    Text(
+                        historyItem.translatedText,
+                        fontSize = 14.sp
+                    )
+                }
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        (context as Activity).apply {
+                            startActivity(
+                                Intent(context, MainActivity::class.java).apply {
+                                    putExtra(
+                                        Intent.EXTRA_TEXT,
+                                        historyItem.insertedText as CharSequence
+                                    )
+                                    setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                                }
+                            )
+                            finishAffinity()
+                        }
+                    }
+                ) {
+                    Text(stringResource(R.string.open))
+                }
+            }
+        )
     }
 }
