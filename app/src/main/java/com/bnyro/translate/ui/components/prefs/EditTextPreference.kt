eda65accad3f9d11980d21a2c4956eb05dacc407
Bnyro <bnyro@tutanota.com>
2022-11-03 11:20:55 +0100

    ktlint

--- app/src/main/java/com/bnyro/translate/ui/components/prefs/EditTextPreference.kt
+++ app/src/main/java/com/bnyro/translate/ui/components/prefs/EditTextPreference.kt
@@ -5,8 +5,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.bnyro.translate.util.Preferences
 
