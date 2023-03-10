d2b9ca8628d56295960a9929a2029782e18b1273
Bnyro <bnyro@tutanota.com>
2023-03-10 15:48:44 +0100

    Fix the position of the options dropdown

--- app/src/main/java/com/bnyro/translate/ui/components/TopBarMenu.kt
+++ app/src/main/java/com/bnyro/translate/ui/components/TopBarMenu.kt
@@ -1,5 +1,6 @@
 package com.bnyro.translate.ui.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
@@ -10,9 +11,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.PopupProperties
 import com.bnyro.translate.obj.MenuItemData
 
 @Composable
@@ -23,28 +22,27 @@ fun TopBarMenu(
         mutableStateOf(false)
     }
 
-    // 3 vertical dots icon
-    StyledIconButton(
-        imageVector = Icons.Default.MoreVert,
-        onClick = {
-            expanded = true
-        }
-    )
+    Box {
+        // 3 vertical dots icon
+        StyledIconButton(
+            imageVector = Icons.Default.MoreVert,
+            onClick = {
+                expanded = true
+            }
+        )
 
-    DropdownMenu(
-        modifier = Modifier.width(width = 150.dp),
-        expanded = expanded,
-        onDismissRequest = {
-            expanded = false
-        },
-        // adjust the position
-        offset = DpOffset(x = (-102).dp, y = (-64).dp),
-        properties = PopupProperties()
-    ) {
-        // adding each menu item
-        menuItems.forEach { menuItemData ->
-            DropDownItem(menuItemData) { newState ->
-                expanded = newState
+        DropdownMenu(
+            modifier = Modifier.width(width = 150.dp),
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+            }
+        ) {
+            // adding each menu item
+            menuItems.forEach { menuItemData ->
+                DropDownItem(menuItemData) { newState ->
+                    expanded = newState
+                }
             }
         }
     }
