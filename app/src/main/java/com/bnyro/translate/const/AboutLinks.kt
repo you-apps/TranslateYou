59fa1c35427abdc10bb76fabe32292e983ec291d
Bnyro <bnyro@tutanota.com>
2023-03-19 10:19:43 +0100

    Fix the link of the GPL-3 license (closes #190)

--- app/src/main/java/com/bnyro/translate/const/AboutLinks.kt
+++ app/src/main/java/com/bnyro/translate/const/AboutLinks.kt
@@ -4,5 +4,5 @@ object AboutLinks {
     const val PRIVACY_POLICY = "https://raw.githubusercontent.com/Bnyro/TranslateYou/master/PRIVACY%20POLICY.md"
     const val AUTHOR = "https://bnyro.github.io"
     const val GITHUB = "https://github.com/Bnyro/TranslateYou"
-    const val GNU = "https://gnu.org/licenses/gpl-3.ß.html"
+    const val GNU = "https://www.gnu.org/licenses/gpl-3.0.en.html"
 }
