4c057ebcb1b904b546ab9221bdab82efde03bce4
Bnyro <bnyro@tutanota.com>
2023-03-27 18:37:53 +0200

    Fix using `Auto` as language in LibreTranslate (closes #202)

---
+++ app/src/main/java/com/bnyro/translate/api/lt/obj/LTDetectedLanguage.kt
@@ -0,0 +1,8 @@
+package com.bnyro.translate.api.lt.obj
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LTDetectedLanguage(
+    val language: String? = null
+)
--- app/src/main/java/com/bnyro/translate/obj/DetectedLanguage.kt
+++
@@ -1,9 +0,0 @@
-package com.bnyro.translate.obj
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class DetectedLanguage(
-    val confidence: Int? = null,
-    val language: String? = null
-)
