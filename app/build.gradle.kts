ed4e1d83a945d569301e49510c26e88aa2358321
doomsdayrs <doomsdayrs@gmail.com>
2023-07-03 08:02:54 -0400

    Convert groovy gradle to kotlin gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,104 @@
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+    id ("org.jetbrains.kotlin.kapt")
+    id ("kotlinx-serialization")
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId= "com.bnyro.translate"
+        minSdk =21
+        targetSdk= 33
+        versionCode =35
+        versionName ="6.2"
+
+        testInstrumentationRunner =  "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary =true
+        }
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments += listOf("room.schemaLocation" to "$projectDir/schemas".toString())
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+
+        debug {
+            isDebuggable =true
+            applicationIdSuffix =".debug"
+        }
+    }
+
+    flavorDimensions.add("version")
+    productFlavors {
+        create("libre") {
+            dimension ="version"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion ="1.4.2"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    namespace= "com.bnyro.translate"
+}
+
+dependencies {
+    // Android Core
+    implementation( "androidx.core:core-ktx:1.10.1")
+    implementation( "androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation( "androidx.activity:activity-compose:1.7.2")
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation( "androidx.test.ext:junit:1.1.5")
+    androidTestImplementation( "androidx.test.espresso:espresso-core:3.5.1")
+
+    // Compose
+    val   compose_version = "1.5.0-beta01"
+    implementation( "androidx.compose.ui:ui:$compose_version")
+    implementation( "androidx.compose.ui:ui-tooling-preview:$compose_version")
+    implementation( "androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation( "androidx.compose.material3:material3:1.2.0-alpha02")
+    implementation( "androidx.compose.material:material-icons-extended:$compose_version")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$compose_version")
+    debugImplementation( "androidx.compose.ui:ui-tooling:$compose_version")
+    debugImplementation( "androidx.compose.ui:ui-test-manifest:$compose_version")
+    implementation ("androidx.navigation:navigation-compose:2.5.3")
+
+    // Retrofit
+    implementation( "com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation( "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // Room database
+    implementation ("androidx.room:room-runtime:2.5.1")
+    kapt ("androidx.room:room-compiler:2.5.1")
+
+    // Tesseract OCR
+    implementation( "cz.adaptech.tesseract4android:tesseract4android-openmp:4.3.0")
+}
