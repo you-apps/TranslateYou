e968458f87e2fdacb1080fbf3eccad1a92eeef62
Bnyro <bnyro@tutanota.com>
2024-12-30 21:10:18 +0100

    fix: force single line api url and api key input

--- app/src/main/java/com/bnyro/translate/ui/components/prefs/EditTextPreference.kt
+++ app/src/main/java/com/bnyro/translate/ui/components/prefs/EditTextPreference.kt
@@ -18,14 +18,15 @@
 package com.bnyro.translate.ui.components.prefs
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import com.bnyro.translate.util.Preferences
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditTextPreference(
     modifier: Modifier = Modifier,
@@ -46,6 +47,8 @@ fun EditTextPreference(
                 text = labelText
             )
         },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
         modifier = modifier
             .fillMaxWidth()
     )
