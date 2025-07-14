a8063c4bbc337cacbc2940bcdfc6dcfaaf15934e
Julien Papasian <papjul@users.noreply.github.com>
2025-07-14 22:26:54 +0200

    Update DeepL extension version and User-Agent

--- app/src/main/java/com/bnyro/translate/api/deepl/DeeplEngine.kt
+++ app/src/main/java/com/bnyro/translate/api/deepl/DeeplEngine.kt
@@ -166,7 +166,7 @@ class DeeplEngine : TranslationEngine(
     } else webUrl
 
     companion object {
-        const val WEB_CHROME_EXTENSION_VER = "1.18.0"
-        const val WEB_CHROME_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+        const val WEB_CHROME_EXTENSION_VER = "1.49.0"
+        const val WEB_CHROME_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
     }
 }
