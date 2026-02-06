ede6e8a9dfd1c588c5d5da392de6345ba391f7b3
KZsolt21 <kosazsolt69@gmail.com>
2026-02-06 18:56:51 +0100

    Add imePadding modifier for better keyboard handling

--- app/src/main/java/com/bnyro/translate/ui/screens/HistoryScreen.kt
+++ app/src/main/java/com/bnyro/translate/ui/screens/HistoryScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -91,7 +92,8 @@ fun HistoryScreen(
 
     Scaffold(
         modifier = Modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection),
+            .nestedScroll(scrollBehavior.nestedScrollConnection)
+            .imePadding(),
         topBar = {
             SearchAppBar(
                 title = stringResource(id = titleId),
--- app/src/main/java/com/bnyro/translate/ui/screens/TranslationPage.kt
+++ app/src/main/java/com/bnyro/translate/ui/screens/TranslationPage.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -90,7 +91,9 @@ fun TranslationPage(
 
     val snackbarHostState = remember { SnackbarHostState() }
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .imePadding(),
         snackbarHost = {
             SnackbarHost(hostState = snackbarHostState)
         },
