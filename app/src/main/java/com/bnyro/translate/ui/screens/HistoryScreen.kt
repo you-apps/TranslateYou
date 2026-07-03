ea207618e4ad3cb8011b3545ade571178a6ff14b
Bnyro <bnyro@tutanota.com>
2026-07-03 21:22:39 +0200

    feat: add support for exporting history to text file (closes #582)

--- app/src/main/java/com/bnyro/translate/ui/screens/HistoryScreen.kt
+++ app/src/main/java/com/bnyro/translate/ui/screens/HistoryScreen.kt
@@ -17,7 +17,11 @@
 
 package com.bnyro.translate.ui.screens
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BasicTooltipBox
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -29,18 +33,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberBasicTooltipState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.DeleteForever
 import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -51,20 +61,24 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupPositionProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.bnyro.translate.R
 import com.bnyro.translate.db.obj.HistoryItemType
+import com.bnyro.translate.ext.toastFromMainThread
 import com.bnyro.translate.ui.components.DialogButton
 import com.bnyro.translate.ui.components.SearchAppBar
 import com.bnyro.translate.ui.components.StyledIconButton
 import com.bnyro.translate.ui.models.HistoryModel
 import com.bnyro.translate.ui.models.TranslationModel
 import com.bnyro.translate.ui.views.HistoryRow
+import kotlin.math.exp
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun HistoryScreen(
     navController: NavController,
@@ -74,6 +88,7 @@ fun HistoryScreen(
     @StringRes clearItemsHintId: Int
 ) {
     val viewModel: HistoryModel = viewModel()
+    val context = LocalContext.current
 
     var showDeleteHistoryDialog by remember {
         mutableStateOf(false)
@@ -86,6 +101,17 @@ fun HistoryScreen(
     LaunchedEffect(Unit) {
         viewModel.fetchItems(itemType)
     }
+
+    val exportFilePicker =
+        rememberLauncherForActivityResult(ActivityResultContracts.CreateDocument("text/plain")) { uri ->
+            if (uri == null) return@rememberLauncherForActivityResult
+
+            context.contentResolver.openOutputStream(uri)?.use { os ->
+                os.write(viewModel.items.joinToString("\n") { it.insertedText }.toByteArray())
+            }
+
+            context.toastFromMainThread(R.string.successfully_exported)
+        }
 
     Scaffold(
         modifier = Modifier
@@ -113,6 +139,27 @@ fun HistoryScreen(
                             contentDescription = stringResource(id = clearItemsHintId)
                         )
                     }
+
+                    TooltipBox(
+                        positionProvider = TooltipDefaults.rememberTooltipPositionProvider(4.dp),
+                        tooltip = {
+                            PlainTooltip() { Text(stringResource(R.string.export_to_text_file)) }
+                        },
+                        state = rememberTooltipState()
+                    ) {
+                        IconButton(onClick = {
+                            exportFilePicker.launch(
+                                "${context.getString(R.string.app_name)}_${
+                                    context.getString(titleId)
+                                }.txt"
+                            )
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Share,
+                                contentDescription = stringResource(R.string.export_to_text_file)
+                            )
+                        }
+                    }
                 }
             )
         },
@@ -125,7 +172,7 @@ fun HistoryScreen(
                 val query = searchQuery.lowercase()
                 val filteredItems = viewModel.items.filter {
                     it.insertedText.lowercase().contains(query) ||
-                        it.translatedText.lowercase().contains(query)
+                            it.translatedText.lowercase().contains(query)
                 }
 
                 if (filteredItems.isNotEmpty()) {
